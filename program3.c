00b0f6134de93be2b642fa00d971ee76d1b3125f
Yorgo Kennos <yorgokennos@gmail.com>
2018-12-14 17:52:22 -0500

    Update program3.c

--- program3.c
+++ program3.c
@@ -1,31 +1,9 @@
 /***********************************************
 Author:	Yorgo A. Kennos
 Date:	December 2, 2018
-Purpose: Program converts inputed lengths and weights to and from Metric and US systems. User can choose the type of conversion through a text menu.
-
-Sources	of	Help:	NONE
-
-Time Spent:	2 Hours
-
-Computing	I	-- COMP.1010	Honor	Statement
-The	practice	of	good	ethical	behavior	is	essential	for	maintaining
-good	order	in	the	classroom,	providing	an	enriching	learning
-experience	for	students,	and	as	training	as	a	practicing	computing
-professional	upon	graduation.		This	practice	is	manifested	in	the
-University's	Academic	Integrity	policy.	Students	are	expected	to
-strictly	avoid	academic	dishonesty	and	adhere	to	the	Academic
-Integrity	policy	as	outlined	in	the	course	catalog.		Violations	will
-be	dealt	with	as	outlined	therein.
-All	programming	assignments	in	this	class	are	to	be	done	by	the
-student	alone.		No	outside	help	is	permitted	except	the	instructor	and
-approved	tutors.
-I	certify that	the	work	submitted	with	this	assignment	is	mine	and	was
-generated	in	a	manner	consistent	with	this	document,	the	course
-academic	policy	on	the	course	website	on	Blackboard,	and	the	UMass
-Lowell	academic	code.
-Date: December 2, 2018
-Name: Yorgo A. Kennos
-
+Purpose: Program converts inputed lengths and 
+weights to and from Metric and US systems. 
+User can choose the type of conversion through a text menu.
 **************************************/
 
 #include <stdio.h>
