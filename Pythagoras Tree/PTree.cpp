7d2c2bfc4174561ccff6700cab1833b6196fb865
Yorgo Kennos <yorgokennos@gmail.com>
2020-02-23 12:39:22 -0500

    Create PTree.cpp

---
+++ Pythagoras Tree/PTree.cpp
@@ -0,0 +1,12 @@
+/*
+
+
+Disclaimer: Due to University Honor Statements I am unable to post my code on the internet. I can however show the output of my algorithm.
+  This was done on 2/23/2020 using the Super Fast Media Library (SFML) on Ubuntu.
+  
+  
+  
+
+
+
+*/
