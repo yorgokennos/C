4b0ac9e8dca178590d3208df7d0095b21a302b79
Yorgo Kennos <yorgokennos@gmail.com>
2020-02-23 12:47:06 -0500

    Create main.cpp

---
+++ ImageEncoder/main.cpp
@@ -0,0 +1,10 @@
+/*
+  Due to University Honor Statement I am unable to post my code.
+  
+  Assignment description: Using a generate function (which XORs fixed tap bits
+    together to determine the next bit after shifting) along with a starting Key
+    to go through each pixel of an input image XOR-ing the RGB components of each 
+    pixel to "encode" the image. 
+    
+    We can decode the image using the encoded image along with our original key
+*/
